321e72252e8a8a7c99650c9eaa4efc2919adfa89
Gummy27 <brimir27@outlook.com>
2021-02-20 15:08:58 +0000

    Eitthvað

---
+++ 2016/lettastaVerkefnid.py
@@ -0,0 +1,15 @@
+from collections import defaultdict
+
+input()
+n = int(input())
+
+problems = defaultdict(lambda x : 0)
+
+for x in list(input().split(" ")):
+    print(x)
+
+
+
+for x in range(n):
+    pass
+    
